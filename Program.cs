6fefef1c9c24cdf1d840bca8c0c607d27c03d720
Leonardo Machado <72328522+MRmachad@users.noreply.github.com>
2023-02-08 23:50:21 -0300

    fist commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using UMBIT.RabbitMQ.Sender;
+using RabbitMQ.Client;
+using System.Runtime.Serialization.Formatters.Binary;
+
+var factory = new ConnectionFactory { HostName = "127.0.0.1"};
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(queue: "hello",
+                     durable: false,
+                     exclusive: false,
+                     autoDelete: false,
+                     arguments: null);
+
+const string message = "Hello World!";
+var t = new Teste() { name = "teste", teste = "teste" };
+
+BinaryFormatter bf = new BinaryFormatter();
+using (MemoryStream ms = new MemoryStream())
+{
+
+    bf.Serialize(ms, t);
+
+
+    channel.BasicPublish(exchange: string.Empty,
+                         routingKey: "hello",
+                         basicProperties: null,
+                         body: ms.ToArray());
+
+    Console.WriteLine($" [x] Sent {message}");
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
+
+
+
---
+++ Teste.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMBIT.RabbitMQ.Sender
+{
+    [Serializable]
+    public class Teste
+    {
+        public string name { get; set; }
+        public string teste { get; set; }
+    }
+
+}
